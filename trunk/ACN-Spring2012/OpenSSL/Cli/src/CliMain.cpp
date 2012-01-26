7c4e44b71816c07c5217105fa785666225ea630f
infinity.ethereal <infinity.ethereal@ef33575d-811c-aa3e-9c56-0e3b2be09934>
2012-01-26 03:55:13 +0000

    [ACN-Spring2012] OpenSSLCli C++ Wrapper: -  Implemented symmetric encryption and decryption. Decryption needs to be implemented in main menu. - Wrote prototype for base64 encode.

--- trunk/ACN-Spring2012/OpenSSL/Cli/src/CliMain.cpp
+++ trunk/ACN-Spring2012/OpenSSL/Cli/src/CliMain.cpp
@@ -1,35 +1,61 @@
+#include <cstdlib>
 #include <OpenSSLCli.h>
 
 int main (int argc, char **argv)
 {
 	char choice;
-	cout << "1. Compute Hash." << endl;
-	cout << "2. Sign a file." << endl;
-	cout << "3. Symmetric Encryption." << endl;
-	cout << "4. Symmetric Decryption." << endl;
-	cout << "Enter a choice: ";
-	cin >> choice;
+	while (true)
+	{	
+		//system ("clear");
+		cout << "===================== Main Menu ========================" << endl;
+		cout << "1. Compute hash (e.g. sha1, md5) of an input file." << endl;
+		cout << "2. Sign a file using RSA/DSA private key." << endl;
+		cout << "3. Symmetric Encryption (e.g. aes, des, des3)." << endl;
+		cout << "4. Symmetric Decryption." << endl;
+		cout << "5. base64 encode." << endl;
+		cout << "e. Exit." << endl;
+		cout << "========================================================" << endl;
+		cout << "Enter a choice: ";
+		cin >> choice;
 	
-	switch (choice)
-	{
-		case '1':
+		switch (choice)
 		{
-			string hashtype, hashfileout, filein;
-			cout << "Enter hash type (e.g. sha1): ";
-			cin >> hashtype;
-			cout << "Enter output hash file ('.' for stdout): ";
-			cin >> hashfileout;
-			cout << "Enter input file: ";
-			cin >> filein;
+			case '1':
+			{
+				string hashtype, hashfileout, filein;
+				cout << "Enter hash type (e.g. sha1): ";
+				cin >> hashtype;
+				cout << "Enter output hash file ('.' for stdout): ";
+				cin >> hashfileout;
+				cout << "Enter input file: ";
+				cin >> filein;
 			
-			OpenSSL_dgst (hashtype, hashfileout, filein);
-			break;
-		}
-		default:
-		{
-			break;
-		}
+				OpenSSL_dgst (hashtype, hashfileout, filein);
+				break;
+			}
+			case '3':
+			{
+				string cipher, plaintextfilein, ciphertextfileout;
+				cout << "Enter cipher with modes (e.g. aes256, des, des3, des3-ede-ofb): ";
+				cin >> cipher;
+				cout << "Enter input plaintext file: ";
+				cin >> plaintextfilein;
+				cout << "Enter output ciphertext file: ";
+				cin >> ciphertextfileout;
+			
+				OpenSSL_enc (cipher, plaintextfilein, ciphertextfileout);
+				break;
+			}
+			case 'e':
+			{
+				return 0;
+			}
+			default:
+			{
+				break;
+			}
 		
+		}
 	}
-	return 0;
+	return -1;
 }
