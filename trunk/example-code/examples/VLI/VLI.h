d40b557a667610caee52d61023a05d9e0d8c78c9
infinity.ethereal <infinity.ethereal@ef33575d-811c-aa3e-9c56-0e3b2be09934>
2012-03-10 09:04:16 +0000

    [example-code] - VLI: Addition/Subtraction implemented.

--- trunk/example-code/examples/VLI/VLI.h
+++ trunk/example-code/examples/VLI/VLI.h
@@ -5,11 +5,27 @@ using std::istream;
 using std::vector;
 class CVLI
 {
-	bool Sign;	// 0=positive, 1=negative
-	vector<unsigned short> Number;
+	bool Sign;			// 0=positive, 1=negative
+	vector<short> Number;
+	void Format ();		// Properly format the Number.
+	bool IsZero () const;		// Is this zero?
+	CVLI Abs () const;
 
 	public:
 	CVLI ();
+	CVLI::CVLI (bool, vector<short> &);		// VLI from sign and number.
+
+	// Comparison operators.
+	bool operator == (const CVLI&);
+	bool operator != (const CVLI&);
+	bool operator < (const CVLI&);
+	bool operator > (const CVLI&);
+	bool operator <= (const CVLI&);
+	bool operator >= (const CVLI&);
+
+	// Addition and Subtraction
+	CVLI operator + (const CVLI&);
+	CVLI operator - (const CVLI&);
 
 	// Input and output.
 	friend ostream& operator << (ostream&, const CVLI&);
