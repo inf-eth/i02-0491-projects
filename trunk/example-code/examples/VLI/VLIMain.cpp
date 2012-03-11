0f8aeb9024f1f8b72da2bf6b211ca9a633e35097
infinity.ethereal <infinity.ethereal@ef33575d-811c-aa3e-9c56-0e3b2be09934>
2012-03-11 14:05:23 +0000

    [example-code] - VLI: Using stringstream for N and M.

--- trunk/example-code/examples/VLI/VLIMain.cpp
+++ trunk/example-code/examples/VLI/VLIMain.cpp
@@ -1,11 +1,30 @@
 #include "VLI.h"
 #include <iostream>
+#include <sstream>
 using std::cin;
 using std::cout;
 using std::endl;
-
+using std::stringstream;
 int main (int argc, char **argv)
 {
+	stringstream ssN, ssM;
+	CVLI N, M;
+	if (argc == 2)
+	{
+		ssN << argv[1];
+		ssN >> N;
+		cout << "N = " << N << endl;
+	}
+	else if (argc == 3)
+	{
+		ssN << argv[1];
+		ssN >> N;
+		ssM << argv[2];
+		ssM >> M;
+		cout << "N = " << N << endl;
+		cout << "M = " << M << endl;
+	}
+	/*
 	CVLI VLIobj1, VLIobj2;
 	cout << "Test VLI." << endl;
 	cin >> VLIobj1;
@@ -21,6 +40,7 @@ int main (int argc, char **argv)
 	cout << "V1 * V2 = " << VLIobj1 * VLIobj2 << endl;
 	cout << "V1 / V2 = " << VLIobj1 / VLIobj2 << endl;
 	cout << "V1 % V2 = " << VLIobj1 % VLIobj2 << endl;
-	cout << "Primality test for V1 = " << VLIobj1.CheckPrime(true) << endl;
+	cout << "Primality test for V1 = " << VLIobj1.CheckPrime(true) << endl;*/
+	cout << "Primality test for M = " << M.CheckPrime(true) << endl;
 	return 0;
 }
