be129bac2cf355208ac0ff45e2b73e94c9e0d18e
infinity.ethereal <infinity.ethereal@ef33575d-811c-aa3e-9c56-0e3b2be09934>
2012-03-27 01:18:10 +0000

    [ACN - Spring 2010] - Added paper presentation pdf.
    [example-code]
    - Modified HighPrecisionTimer code to demonstrate benchmarking.

--- trunk/example-code/examples/HighPrecisionTimer/HighPrecisionTimer.cpp
+++ trunk/example-code/examples/HighPrecisionTimer/HighPrecisionTimer.cpp
@@ -8,5 +8,16 @@ int main (int argc, char **argv)
 	std::cout << "ms = " << ms << std::endl;
 	std::cout << "us = " << us << std::endl;
 
+	// Benchmarking.
+	__int64 tStart, tEnd;
+	tStart = GetTimeus64();
+
+	for (int i=0; i<1024*1024*1000; i++)
+		for (int j=0; j<10; j++)
+			double x = 2.34 * 332.2212 / 32342.2 / 32.234 * 432.2131;
+
+	tEnd = GetTimeus64();
+	std::cout << "Time taken = " << ((double)(tEnd-tStart))/(1000000.) << " seconds." << std::endl;
+
 	return 0;
 }
