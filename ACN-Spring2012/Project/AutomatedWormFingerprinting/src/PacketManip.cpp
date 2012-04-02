9e20447b0378b479d36c2b4a59256e0677d3e2a4
infinity.ethereal <infinity.ethereal@ef33575d-811c-aa3e-9c56-0e3b2be09934>
2012-04-02 04:45:54 +0000

    [ACN - Spring 2010] Automated Worm Fingerprinting: - Added Readme.txt containing instructions to compile and run on Linux or windows. - Added an initialization function for CPacketManip. - Moved header length checks into packet processing structure. - Device name and filter string can be passed as arguments.

--- ACN-Spring2012/Project/AutomatedWormFingerprinting/src/PacketManip.cpp
+++ ACN-Spring2012/Project/AutomatedWormFingerprinting/src/PacketManip.cpp
@@ -45,6 +45,39 @@ CPacketManip::CPacketManip (const char *pdev, const char *pfilter)
 	cout << "Filter: " << pfilter << endl;
 }
 
+void CPacketManip::Initialize (const char *pdev, const char *pfilter)
+{
+	// ask pcap for the network address and mask of the device
+	if (pcap_lookupnet (pdev, &netp, &maskp, errbuf) == -1)
+	{
+		cerr << "ERROR pcap_lookupnet(): " << errbuf << endl;
+		exit(-1);
+	}
+
+	// open device for reading this time lets set it in promiscuous mode so we can monitor traffic to another machine
+	descr = pcap_open_live (pdev, BUFSIZ, 1, -1, errbuf);
+	if(descr == NULL)
+	{
+		cerr << "ERROR pcap_open_live(): " << errbuf << endl;
+		exit(-1);
+	}
+
+	// Lets try and compile the program.. non-optimized
+	if (pcap_compile (descr, &fp, pfilter, 0, netp) == -1)
+	{
+		cerr << "ERROR calling pcap_compile()" << endl;
+		exit(-1);
+	}
+
+	// set the compiled program as the filter
+	if (pcap_setfilter (descr, &fp) == -1)
+	{
+		cerr << "ERROR setting filter" << endl;
+		exit(1);
+	}
+	cout << "Device: " << pdev << endl;
+	cout << "Filter: " << pfilter << endl;
+}
 void CPacketManip::Loop ()
 {
 	pcap_loop (descr, -1, packet_capture_callback, NULL);
@@ -53,43 +86,23 @@ void CPacketManip::Loop ()
 // All the work needs to be done here.
 void packet_capture_callback(u_char *useless,const struct pcap_pkthdr* header,const u_char* packet)
 {
+	if (!header)
+		return;
+	
 	// TODO: Comment this to stop some spam onscreen or set the appropriate filter program in main() if running on network.
 	cout << " Recieved a packet at: " << ctime((const time_t*)&header->ts.tv_sec) << endl;
 
 	// Header pointers.
-	const struct sniff_ethernet *ethernet; /* The ethernet header */
-	const struct sniff_ip *ip; /* The IP header */
-	const struct sniff_tcp *tcp; /* The TCP header */
-	const u_char *payload; /* Packet payload */
-
-	u_int size_ip;		// IP header length.
-	u_int size_tcp;		// TCP header legnth.
+	const struct sniff_ethernet *ethernet = NULL; /* The ethernet header */
+	const struct sniff_ip *ip = NULL; /* The IP header */
+	const struct sniff_tcp *tcp = NULL; /* The TCP header */
+	const u_char *payload = NULL; /* Packet payload */
 
 	// ************************** Pointer Initialization *********************************
 	// packet* is the starting address of captured packet stored in memory.
 	ethernet = (struct sniff_ethernet*)(packet);			// First thing in a packet is ethernet header.
 	ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);		// IP header comes after ethernet header.
-
-	size_ip = IP_HL(ip)*4;		// IP_HL is a macro for ((ip)->ip_vhl) & 0x0f;
-	// IP_HL(ip) gives length in 32bit words. Multiplication by 4 gives length in bytes.
-	if (size_ip < 20)
-	{
-		cerr << "ERROR: Invalid IP header length: " << size_ip << " bytes." << endl;
-		return;
-	}
-
-	tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);		// TCP header follows ethernet and IP headers.
-	size_tcp = TH_OFF(tcp)*4;	// TH_OFF(tcp) is a macro for ((ip)->ip_vhl) >> 4;
-	// TH_OFF(tcp) gives length of TCP header in 32bit words. Multiplication by 4 gives length in bytes.
-	if (size_tcp < 20)
-	{
-		cerr << "ERROR: Invalid TCP header length: " << size_tcp << " bytes." << endl;
-		return;
-	}
-
-	payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);	// Payload or data in packet.
 	// *************************************************************************************
-
 
 	// TODO: Packet capturing condition. Only process packets with the specified protocol, and port numbers.
 	// TODO: Process UDP packets and display their headers.
@@ -97,10 +110,30 @@ void packet_capture_callback(u_char *useless,const struct pcap_pkthdr* header,co
 	// ntohs(tcp->th_sport) gives source port
 	// ntohs(tcp->th_dport) gives destination port
 	// Protocols are IPPROTO_IP, IPPROTO_UDP, IPPROTO_ICMP etc.
-	if ( ip->ip_p == IPPROTO_TCP && (ntohs(tcp->th_sport) == 6000 || ntohs(tcp->th_sport) == 6001) )
+	if ( ip && ip->ip_p == IPPROTO_TCP && (ntohs(tcp->th_sport) == 6000 || ntohs(tcp->th_sport) == 6001) )
 	{
 		// TODO: Process the headers. Display IP header.
+		u_int size_ip;		// IP header length.
+		u_int size_tcp;		// TCP header legnth.
 		cout << " Recieved a TCP packet at: " << ctime((const time_t*)&header->ts.tv_sec) << endl;
+
+		size_ip = IP_HL(ip)*4;		// IP_HL is a macro for ((ip)->ip_vhl) & 0x0f;
+		// IP_HL(ip) gives length in 32bit words. Multiplication by 4 gives length in bytes.
+		if (size_ip < 20)
+		{
+			cerr << "ERROR: Invalid IP header length: " << size_ip << " bytes." << endl;
+			return;
+		}
+
+		tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);		// TCP header follows ethernet and IP headers.
+		size_tcp = TH_OFF(tcp)*4;	// TH_OFF(tcp) is a macro for ((ip)->ip_vhl) >> 4;
+		// TH_OFF(tcp) gives length of TCP header in 32bit words. Multiplication by 4 gives length in bytes.
+		if (size_tcp < 20)
+		{
+			cerr << "ERROR: Invalid TCP header length: " << size_tcp << " bytes." << endl;
+			return;
+		}
+		payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);	// Payload or data in packet.
 
 		/* define/compute tcp payload (segment) offset */
 		//payload = (packet + SIZE_ETHERNET + size_ip + size_tcp);
