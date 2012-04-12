2f23d7806209e943d083dbaaab12f8230039fbb3
infinity.ethereal <infinity.ethereal@ef33575d-811c-aa3e-9c56-0e3b2be09934>
2012-04-12 12:51:01 +0000

    [example-code] - Added symmetric encryption examples using DES (block cipher) and RC4 (stream cipher).

---
+++ example-code/examples/SymmetricEncryption/RC4/RC4.cpp
@@ -0,0 +1,46 @@
+// References:
+// http://www.codealias.info/technotes/des_encryption_using_openssl_a_simple_example
+// http://linux.die.net/man/3/rc4_set_key
+// http://old.nabble.com/About-RC4-decryption.-td32757842.html
+
+#include <unistd.h>
+#include <cstring>
+#include <iostream>
+#include <openssl/rc4.h>
+
+using std::cout;
+using std::endl;
+
+char* Encrypt(unsigned char *keydata, char *plaintext, int Length)
+{
+	char *encrypted = new char[Length+1];
+	RC4_KEY key;
+	RC4_set_key(&key, 8, keydata);
+
+	// Encryption
+	RC4(&key, Length, (const unsigned char*)plaintext, (unsigned char*)encrypted);
+
+	return encrypted;
+}
+
+int main()
+{
+	unsigned char keydata[] = "password";
+	char plaintext[]="This is a secret message.";
+
+	int Length = strlen(plaintext);
+
+	char *Encrypted = Encrypt(keydata, plaintext, Length);
+	char *Decrypted = Encrypt(keydata, Encrypted, Length);
+
+	cout << "Plaintext: " << plaintext << endl;
+
+	cout << "Encrypted: ";
+	for (int i=0; i<Length; i++)
+		cout << Encrypted[i];
+	cout << endl;
+
+	cout << "Decrypted: " << Decrypted << endl;
+
+	return 0;
+}
