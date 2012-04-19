c323dab903dd0293f163f535d1daeb063575b539
infinity.ethereal <infinity.ethereal@ef33575d-811c-aa3e-9c56-0e3b2be09934>
2012-04-19 08:25:15 +0000

    [example-code] - LZ77: Commandline mode.

--- example-code/examples/LZ77/LZ77Main.cpp
+++ example-code/examples/LZ77/LZ77Main.cpp
@@ -1,10 +1,29 @@
+#include <cstring>
 #include <iostream>
 #include <LZ77.h>
 
+using std::cerr;
+using std::endl;
+
 int main (int argc, char **argv)
 {
+	if (argc != 4)
+	{
+		cerr << "USAGE: " << endl
+			 << "Encode: ./LZ77 -e <input file> <output file>" << endl
+			 << "Decode: ./LZ77 -d <input file> <output file>" << endl;
+		return -1;
+	}
+
 	CLZ77 Test;
-	Test.Encode("test.txt", "test.encoded.txt");
-	Test.Decode("test.encoded.txt", "test.decoded.txt");
+	if (strcmp(argv[1], "-e") == 0)
+		Test.Encode(argv[2], argv[3]);
+	else if (strcmp(argv[1], "-d") == 0)
+		Test.Decode(argv[2], argv[3]);
+	else
+	{
+		cerr << "ERROR: Unexpected error. Check arguments and input files." << endl;
+		return -1;
+	}
 	return 0;
 }
