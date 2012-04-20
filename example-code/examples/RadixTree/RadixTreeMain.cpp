b90ffaca456464eb880bf6c5a946a0bd6e78b76f
infinity.ethereal <infinity.ethereal@ef33575d-811c-aa3e-9c56-0e3b2be09934>
2012-04-20 14:25:26 +0000

    [example-code] RadixTree: - Rewrote CRadixTree class as a template.

--- example-code/examples/RadixTree/RadixTreeMain.cpp
+++ example-code/examples/RadixTree/RadixTreeMain.cpp
@@ -7,18 +7,21 @@ using std::endl;
 
 int main(int argc, char **argv)
 {
-	CRadixTree Tree;
+	CRadixTree<int> Tree;
 	Tree.CreateTree();
-	int *paytest = new int;
-	*paytest = 3;
-	int *paysearch = NULL;
+
+	int paytest = 3;
+	int paysearch;
 	int ComparisonCounter = 0;
-	Tree.Insert("testword", strlen("testword"), (void *)paytest);
-	paysearch = (int*)Tree.Search("testword", strlen("testword"), ComparisonCounter);
+
+	Tree.Insert("testword", strlen("testword"), paytest);
+	paysearch = *Tree.Search("testword", strlen("testword"), ComparisonCounter);
+
 	if (paysearch == NULL)
 		cout << "Search unsuccessful." << endl;
 	else
-		cout << "Found value of testword: " << *paysearch << endl;
+		cout << "Found value of testword: " << paysearch << endl;
+
 	Tree.ResetTree();
 	return 0;
 }
