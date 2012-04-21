f85899a7b71d23c3555eb4514f37262ce38db1f1
infinity.ethereal <infinity.ethereal@ef33575d-811c-aa3e-9c56-0e3b2be09934>
2012-04-21 08:40:41 +0000

    [ACN - Spring 2012] Automated Worm Fingerprinting: - Updated TODO list. - Added a benchmark for Vectors Vs RadixTree. Vectors are far more efficient according to this benchmark.

---
+++ ACN-Spring2012/Project/Benchmarking/VectorVsRadixTree/RadixTree.h
@@ -0,0 +1,368 @@
+// RadixTree.h: interface for the CRadixTree class.
+//
+//////////////////////////////////////////////////////////////////////
+#include <cstdlib>
+#pragma once
+
+// The Standard Radix Tree Node.
+template<class T> class RadixTreeNode
+{
+public:
+	unsigned char Label;
+	short NoOfNodes;
+	short Status;
+	short Width;
+	T Payload;
+
+	// Pointers.
+	RadixTreeNode<T> *ParentPTR;
+	RadixTreeNode<T> *NextNodePTRS[256];
+};
+
+// *** The Radix Tree Class Definition ***
+template<class T> class CRadixTree
+{
+public:
+	RadixTreeNode<T> *RootPTR;
+
+	// *** Constructor/Destructor ***
+	CRadixTree();
+	virtual ~CRadixTree();
+
+	// ************************ Public Functions **************************
+	void CreateTree();
+	void ResetTree();
+	
+	// *** Container Functions ***
+	int Insert(const unsigned char *, int, T&);
+	T* Search(const unsigned char *, int, int&);
+	int Delete(const unsigned char *, int);
+	// ****************************
+	// ********************************************************************
+private:
+	int NoOfNodes;
+
+	// *** Private Mutator ( Whatever ) Functions ***
+	void SetWidthForNewNode(RadixTreeNode<T> *);
+	RadixTreeNode<T>* GetMaximumNodeChild(RadixTreeNode<T> *);
+	int  CalculateWidth(RadixTreeNode<T> *);
+
+	void DeleteTree(RadixTreeNode<T> *);
+	void DeleteNode(RadixTreeNode<T> *);
+	// **********************************************
+};
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+template<class T> CRadixTree<T>::CRadixTree()
+{
+	RootPTR = NULL;
+	NoOfNodes = 0;
+}
+
+template<class T> CRadixTree<T>::~CRadixTree()
+{
+	DeleteTree(RootPTR);
+	RootPTR = NULL;
+	NoOfNodes = 0;
+}
+
+// *********************************************************************************
+// ****************** Radix Tree Dictionary's Function Implementation **************
+// *********************************************************************************
+
+template<class T> void CRadixTree<T>::CreateTree()
+{
+	RootPTR = new RadixTreeNode<T>;
+
+	(*RootPTR).Label = 'R';
+	(*RootPTR).NoOfNodes = 0;
+	(*RootPTR).Status = -1;
+	(*RootPTR).ParentPTR = NULL;
+	(*RootPTR).Width = 1;
+
+	for (int i=0; i<256; i++)
+		(*RootPTR).NextNodePTRS[i] = NULL;
+
+	NoOfNodes = 0;
+}
+
+// *** Insertion in the Radix Tree ***
+template<class T> int CRadixTree<T>::Insert(const unsigned char *WordArray, int Length, T &Payload)
+{
+	RadixTreeNode<T> *TempPTR = RootPTR;
+
+	for (int i=0; i<Length; i++)
+	{
+		if (i == (Length-1))
+		{
+			if ((*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]] == NULL)
+			{
+				RadixTreeNode<T> *NewNode = new RadixTreeNode<T>;
+				(*NewNode).Label = WordArray[i];
+				(*NewNode).NoOfNodes = 0;
+				(*NewNode).Status = 1;
+				(*NewNode).Payload = Payload;
+				(*NewNode).ParentPTR = TempPTR;
+				(*TempPTR).NoOfNodes++;
+
+				for (int j=0; j<256; j++)
+					(*NewNode).NextNodePTRS[j] = NULL;
+
+				(*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]] = NewNode;
+				SetWidthForNewNode(NewNode);
+				NoOfNodes++;
+
+				if ((*(*NewNode).ParentPTR).Status == 1)
+					( *(*NewNode).ParentPTR).Status = 2;
+
+				return 0;
+			}
+			else
+			{
+				(*(*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]]).Payload = Payload;
+				if ((*(*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]]).Status == 0 )
+					(*(*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]]).Status = 2;
+
+				return 0;
+			}
+		}
+		else
+		{
+			if ((*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]] == NULL)
+			{
+				RadixTreeNode<T> *NewNode = new RadixTreeNode<T>;
+				(*NewNode).Label = WordArray[i];
+
+				(*NewNode).NoOfNodes = 0;
+				(*NewNode).Status = 0;
+				(*NewNode).ParentPTR = TempPTR;
+				(*TempPTR).NoOfNodes++;
+				
+				for (int k=0; k<256; k++)
+					(*NewNode).NextNodePTRS[k] = NULL;
+
+				(*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]] = NewNode;
+				SetWidthForNewNode(NewNode);
+				NoOfNodes++;
+				
+				TempPTR = (*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]];
+
+				if ((*(*NewNode).ParentPTR).Status == 1)
+					(*(*NewNode).ParentPTR).Status = 2;
+			}
+			else
+				TempPTR = (*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]];
+		}
+	}
+	return -1;
+}
+
+// *** Search in the Radix Tree ***
+template<class T> T* CRadixTree<T>::Search(const unsigned char* WordArray, int Length, int &ComparisonCounter)
+{
+	RadixTreeNode<T> *TempPTR = RootPTR;
+
+	for (int i=0; i<Length; i++)
+	{
+		ComparisonCounter++;
+		if ((*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]] == NULL)
+			return NULL;
+
+		if ((*(*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]]).Label != WordArray[i])
+			return NULL;
+
+		if (i == Length-1)
+		{
+			if ((*(*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]]).Status == 1 || (*(*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]]).Status == 2)
+				return &(*(*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]]).Payload;
+		}
+
+		if ((*(*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]]).Label == WordArray[i])
+			TempPTR = (*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]];
+		else
+			return NULL;
+	}
+	return NULL;
+}
+
+// *** Deletion in the Radix Tree ***
+template<class T> int CRadixTree<T>::Delete(const unsigned char* WordArray, int Length)
+{
+	RadixTreeNode<T> *TempPTR = RootPTR;
+
+	for (int i=0; i<Length; i++)
+	{
+		if ((*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]] == NULL)
+			return -1;
+
+		if ((*(*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]]).Label != WordArray[i])
+			return -1;
+
+		if (i == Length-1)
+		{
+			if ((*(*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]]).Status == 1 || (*(*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]] ).Status == 2)
+			{
+				/*
+				Call the delete this node function
+				*/
+				RadixTreeNode<T> *DELTempPTR;
+				TempPTR = (*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]];
+
+				if ( (*TempPTR).Status == 2 )
+				{
+					if ((*TempPTR).Payload != NULL)
+						//delete (*TempPTR).Payload;
+					(*TempPTR).Payload = NULL;
+					(*TempPTR).Status = 0;
+					return 0;
+				}
+				else if ((*TempPTR).Status == 1)
+				{
+					while (TempPTR != RootPTR && (*TempPTR).Status != 2 && (*TempPTR).NoOfNodes == 0)
+					{
+						DELTempPTR = TempPTR;
+						(*(*TempPTR).ParentPTR ).NoOfNodes--;
+						(*(*TempPTR).ParentPTR ).NextNodePTRS[(int)(*TempPTR).Label] = NULL;
+
+						TempPTR = (*TempPTR).ParentPTR;
+						if (DELTempPTR->Payload != NULL)
+							//delete DELTempPTR->Payload;
+						delete DELTempPTR;
+						NoOfNodes--;
+
+						SetWidthForNewNode (TempPTR);
+					}
+					if ( (*TempPTR).Status == 2 )
+						(*TempPTR).Status = 1; 
+
+					return 0;
+				}
+				return 0;
+			}
+		}
+
+		if ((*(*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]]).Label == WordArray[i] )
+			TempPTR = (*TempPTR).NextNodePTRS[(unsigned short)WordArray[i]];
+		else
+			return -1;
+	}
+	return -1;
+}
+
+// *** Reset the Radix Tree ***
+template<class T> void CRadixTree<T>::ResetTree()
+{
+	if (RootPTR == NULL)
+		return;
+
+	for (int i=0; i<256; i++)
+	{
+		if ((*RootPTR).NextNodePTRS[i] != NULL)
+			DeleteTree((*RootPTR).NextNodePTRS[i]);
+	}
+}
+
+// ***********************************************************************************
+// ************************** Private Mutator Functions ******************************	
+// ***********************************************************************************
+template<class T> void CRadixTree<T>::SetWidthForNewNode(RadixTreeNode<T> *NodePTR)
+{
+	RadixTreeNode<T> *TempPTR = NodePTR;
+
+	while (TempPTR != NULL)
+	{
+		(*TempPTR).Width = CalculateWidth(TempPTR);
+		TempPTR = (*TempPTR).ParentPTR;
+	}
+}
+
+template<class T> RadixTreeNode<T>* CRadixTree<T>::GetMaximumNodeChild(RadixTreeNode<T> *NodePTR)
+{
+	RadixTreeNode<T> *TempPTR = NULL; 
+	int Max = 1;
+
+	if (NodePTR == NULL)
+		return TempPTR;
+
+	if ((*NodePTR).NoOfNodes == 0)
+		return TempPTR;
+	else
+	{
+		for (int i=0; i<256; i++)
+		{
+			if ((*NodePTR).NextNodePTRS[i] != NULL)
+			{
+				if ((*(*NodePTR).NextNodePTRS[i] ).NoOfNodes > Max)
+				{
+					Max = (*(*NodePTR).NextNodePTRS[i]).NoOfNodes;
+					TempPTR = (*NodePTR).NextNodePTRS[i];
+				}
+			}
+		}
+		return TempPTR;
+	}
+	return TempPTR;
+}
+
+template<class T> int CRadixTree<T>::CalculateWidth(RadixTreeNode<T> *NodePTR)
+{
+	if ((*NodePTR).NoOfNodes == 0)
+		return 1;
+	else
+	{
+		int Temp = 0;
+		for (int i=0; i<256; i++)
+		{
+			if ((*NodePTR).NextNodePTRS[i] != NULL)
+				Temp = Temp + (*(*NodePTR).NextNodePTRS[i]).Width;
+		}
+		return Temp;
+	}
+	return 1;
+}
+
+template<class T> void CRadixTree<T>::DeleteTree(RadixTreeNode<T> *RootPTR1)
+{
+	// Base Cases.
+	if (RootPTR1 == NULL)
+		return;
+
+	if ((*RootPTR1).NoOfNodes == 0)
+	{
+		if ((*RootPTR1).ParentPTR == NULL)
+		{
+			NoOfNodes--;
+			delete RootPTR1;
+
+			return;
+		}
+		else
+		{
+			(*(*RootPTR1).ParentPTR).NextNodePTRS[(int)(*RootPTR1).Label] = NULL;
+			(*(*RootPTR1).ParentPTR).NoOfNodes--;
+			SetWidthForNewNode((*RootPTR1).ParentPTR);
+
+			NoOfNodes--;
+			delete RootPTR1;
+
+			return;
+		}
+	}
+	// Else if it is not the base case.
+	else
+	{
+		for (int i=0; i<256; i++)
+		{
+			if ((*RootPTR1).NextNodePTRS[i] != NULL)
+				DeleteTree((*RootPTR1).NextNodePTRS[i]);
+		}
+		DeleteTree(RootPTR1);
+		return;
+	}
+}
+// ***********************************************************************************
+// ************************ Private Mutator Functions End ****************************
+// ***********************************************************************************
---
+++ ACN-Spring2012/Project/Benchmarking/VectorVsRadixTree/VectorVsRadixTree.cpp
@@ -0,0 +1,150 @@
+#include <Timing.h>
+#include <RadixTree.h>
+
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <openssl/sha.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+#define KEY_LENGTH 20
+#define INSERTIONS 1024*16
+
+struct ContentPrevalenceEntry
+{
+	unsigned char Key[KEY_LENGTH];
+	int Count;
+	__int64 InsertionTime;
+};
+
+// Functions.
+unsigned char * GenerateKey (unsigned char *, unsigned int);
+int SearchContentPrevalenceTable (unsigned char *);
+// Compare two blocks of memory and returns true if they match else returns false.
+bool memncmp (const char *, const char *, int);
+// Memcopy.
+void memncpy (char *, const char *, int);
+
+// Globals.
+vector<ContentPrevalenceEntry> ContentPrevalenceTable;
+CRadixTree<int> Tree;
+__int64 tStart, tEnd;
+vector<string> RandomStrings;
+vector<unsigned char*> Keys;
+
+int main (int argc, char **argv)
+{
+	// Random string generation.
+	srand((unsigned int)time(NULL));
+
+	tStart = GetTimeus64();
+	for (int i=0; i<INSERTIONS; i++)
+	{
+		int size = rand()%48+2;
+		char *temp = new char[size];
+		for (int j=0; j<size; j++)
+			temp[j] = (char)(rand()%25+65);
+		temp[size-1] = '\0';
+		RandomStrings.push_back(temp);
+		delete []temp;
+	}
+	tEnd = GetTimeus64();
+	cout << "Time taken for random generation of " << INSERTIONS << " strings: " << ((double)(tEnd-tStart))/(1000000.) << " seconds." << endl;
+
+	// Key generation.
+	tStart = GetTimeus64();
+	for (int i=0; i<INSERTIONS; i++)
+	{
+		unsigned char *temp = GenerateKey((unsigned char *)RandomStrings[i].c_str(), (unsigned int)RandomStrings[i].size());
+		Keys.push_back(temp);
+	}
+	tEnd = GetTimeus64();
+	cout << "Time taken for random generation of " << INSERTIONS << " keys: " << ((double)(tEnd-tStart))/(1000000.) << " seconds." << endl;
+
+	// Unique insertions in Address Dispersion Table vector.
+	tStart = GetTimeus64();
+	int SearchIndex;
+	ContentPrevalenceEntry temp;
+	for (int i=0; i<INSERTIONS; i++)
+	{
+		SearchIndex = SearchContentPrevalenceTable (Keys[i]);
+		if (SearchIndex != -1)
+			ContentPrevalenceTable[SearchIndex].Count++;
+		else
+		{
+			memncpy((char *)temp.Key, (const char *)Keys[i], KEY_LENGTH);
+			temp.InsertionTime = GetTimeus64();
+			temp.Count = 1;
+			ContentPrevalenceTable.push_back(temp);
+		}
+	}
+	tEnd = GetTimeus64();
+	cout << "Time taken for " << INSERTIONS << " insertions in vector: " << ((double)(tEnd-tStart))/(1000000.) << " seconds." << endl;
+
+	// Unique insertions in Radix Tree.
+	tStart = GetTimeus64();
+	Tree.CreateTree();
+	int *RadixSearch;
+	for (int i=0; i<INSERTIONS; i++)
+	{
+		int ComparisonCounter = 0;
+		RadixSearch = Tree.Search((const unsigned char *)Keys[i], KEY_LENGTH, ComparisonCounter);
+		if (RadixSearch == NULL)
+		{
+			int Count = 1;
+			Tree.Insert((const unsigned char *)Keys[i], KEY_LENGTH, Count);
+		}
+		else
+			(*RadixSearch)++;
+	}
+	tEnd = GetTimeus64();
+	cout << "Time taken for " << INSERTIONS << " insertions in radix tree: " << ((double)(tEnd-tStart))/(1000000.) << " seconds." << endl;
+
+	return 0;
+}
+
+// Search Key in Content Prevalence Table.
+int SearchContentPrevalenceTable (unsigned char *pKey)
+{
+	for (int i=0; i < (int)ContentPrevalenceTable.size(); i++)
+	{
+		if (memncmp ((const char *)ContentPrevalenceTable[i].Key, (const char *)pKey, KEY_LENGTH) == true)
+			return i;
+	}
+	return -1;
+}
+
+// Generate Key (sha1 hash).
+unsigned char * GenerateKey (unsigned char *ppayload, unsigned int psize_payload)
+{
+	unsigned char *Key = new unsigned char[KEY_LENGTH];
+	unsigned char *Input = new unsigned char[psize_payload];
+
+	SHA1(ppayload, psize_payload, Key);
+	return Key;
+}
+
+// Compare two blocks of memory and returns true if they match else returns false.
+bool memncmp (const char *block1, const char *block2, int size)
+{
+	for (int i=0; i<size; i++)
+	{
+		if (block1[i] != block2[i])
+			return false;
+	}
+	return true;
+}
+
+void memncpy (char *dst, const char *src, int size)
+{
+	for (int i=0; i<size; i++)
+		dst[i] = src[i];
+}
