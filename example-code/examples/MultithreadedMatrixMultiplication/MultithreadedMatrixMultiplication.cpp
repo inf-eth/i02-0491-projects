8cdb85eaeefccd348efdc76f4b27cb6b3d9fec23
infinity.ethereal <infinity.ethereal@ef33575d-811c-aa3e-9c56-0e3b2be09934>
2013-09-08 18:15:05 +0000

    [example-code] - Added multi-threaded matrix multiplication example.

---
+++ example-code/examples/MultithreadedMatrixMultiplication/MultithreadedMatrixMultiplication.cpp
@@ -0,0 +1,203 @@
+#if defined __linux__ || defined __CYGWIN__
+#include <pthread.h>
+#define TRET_TYPE void*
+#include <sys/time.h>
+#else
+#include <process.h>
+#include <Windows.h>
+#include <Timer.h>
+#define TRET_TYPE void
+#endif
+
+#include <Timer.h>
+#include <ctime>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+//#include <sys/types.h>
+using namespace std;
+
+#define DataType double
+const int Rows = 1024;
+const int Cols = 1024;
+
+TRET_TYPE MultiplicationThread (void *);
+
+#define Matrix(i,j) Matrix_[Cols*(i)+(j)]
+#define MatrixA(i,j) MatrixA_[Cols*(i)+(j)]
+#define MatrixB(i,j) MatrixB_[Cols*(i)+(j)]
+#define MatrixC(i,j) MatrixC_[Cols*(i)+(j)]
+
+class Argument
+{
+	public:
+	unsigned int GlobalThreads;
+	unsigned int ThreadID;
+	int Rows;
+	int Cols;
+	DataType* MatA_;
+	DataType* MatB_;
+	DataType* MatC_;
+};
+
+void Input(DataType* Matrix_)
+{
+	for (int i=0; i<Rows; i++)
+	{
+		cout << "Row " << i << endl;
+		for (int j=0; j<Cols; j++)
+			cin >> Matrix(i,j);
+	}
+}
+
+void InputRandom(DataType* Matrix_)
+{
+	for (int i=0; i<Rows; i++)
+	{
+		for (int j=0; j<Cols; j++)
+			Matrix(i,j) = (DataType)rand()/rand();
+	}
+}
+
+void Display(DataType* Matrix_)
+{
+	for (int i=0; i<Rows; i++)
+	{
+		for (int j=0; j<Cols; j++)
+			cout << setprecision(3) << setw(5) << Matrix(i,j) << "\t";
+		cout << endl;
+	}
+}
+
+void Initialise(DataType* Matrix_)
+{
+	for (int i=0; i<Rows; i++)
+	{
+		for (int j=0; j<Cols; j++)
+			Matrix(i,j) = (DataType)0;
+	}
+}
+
+void Multiply(DataType* MatrixA_, DataType* MatrixB_, DataType* MatrixC_)
+{
+	for (int i=0; i<Rows; i++)
+	{
+		for (int j=0; j<Cols; j++)
+		{
+			for (int k=0; k<Cols; k++)
+				MatrixC(i,j) = MatrixC(i,j) + MatrixA(i,j) * MatrixB(i,j);
+		}
+	}
+}
+
+int main()
+{
+	int NoOfThreads;
+	cout << "Matrix dimensions are: " << Rows << "x" << Cols << endl;
+	cout << "Enter number of threads to spawn: ";
+	cin >> NoOfThreads;
+
+	#if defined __linux__ || defined __CYGWIN__
+	std::cout << "Running on linux..." << std::endl;
+	pthread_t* threads = new pthread_t[NoOfThreads];
+	#else
+	std::cout << "Running on windows..." << std::endl;
+	HANDLE* th = new HANDLE[NoOfThreads];
+	#endif
+
+	srand((unsigned int)time(NULL));
+	DataType* MatrixA_ = new DataType[Rows*Cols];
+	DataType* MatrixB_ = new DataType[Rows*Cols];
+	DataType* MatrixC_ = new DataType[Rows*Cols];
+
+	InputRandom(MatrixA_);
+	InputRandom(MatrixB_);
+	Initialise(MatrixC_);
+
+	Argument* Args = new Argument[NoOfThreads];
+	//cout << "Matrix A is :" << endl;
+	//Display(MatrixA);
+	//cout << "Matrix B is :" << endl;
+	//Display(MatrixB);
+
+	__int64 tStart, tEnd;
+	tStart = GetTimeus64();
+
+	#if defined __linux__ || defined __CYGWIN__
+	for (int i=0; i<NoOfThreads; i++)
+	{
+		Args[i].GlobalThreads = NoOfThreads;
+		Args[i].Rows = Rows;
+		Args[i].Cols = Cols;
+		Args[i].MatA_ = MatrixA_;
+		Args[i].MatB_ = MatrixB_;
+		Args[i].MatC_ = MatrixC_;
+		Args[i].ThreadID = i;
+		pthread_create (&threads[i], NULL, MultiplicationThread, (void*)&Args[i]);
+	}
+	for (int i=0; i<NoOfThreads; i++)
+		pthread_join (threads[i], NULL);
+	#else
+	for (int i=0; i<NoOfThreads; i++)
+	{
+		Args[i].GlobalThreads = NoOfThreads;
+		Args[i].Rows = Rows;
+		Args[i].Cols = Cols;
+		Args[i].MatA_ = MatrixA_;
+		Args[i].MatB_ = MatrixB_;
+		Args[i].MatC_ = MatrixC_;
+		Args[i].ThreadID = i;
+		th[i] = (HANDLE)_beginthread (MultiplicationThread, 0, (void*)&Args[i]);
+	}
+	WaitForMultipleObjects (NoOfThreads, th, NULL, INFINITE);
+	#endif
+
+	tEnd = GetTimeus64();
+	std::cout << "Time taken = " << ((double)(tEnd-tStart))/(1000000.) << " seconds." << std::endl;
+
+	// Single threaded implementation.
+	Initialise(MatrixC_);
+
+	tStart = GetTimeus64();
+	Multiply(MatrixA_, MatrixB_, MatrixC_);
+	tEnd = GetTimeus64();
+	std::cout << "Time taken (single-threaded) = " << ((double)(tEnd-tStart))/(1000000.) << " seconds." << std::endl;
+
+	//cout << "Matrix C is :" << endl;
+	//Display(MatrixC);
+
+	delete[] MatrixA_;
+	delete[] MatrixB_;
+	delete[] MatrixC_;
+
+	return 0;
+}
+
+TRET_TYPE MultiplicationThread (void *args)
+{
+	unsigned int GlobalThreads = ((Argument*)args)->GlobalThreads;
+	unsigned int ThreadID = ((Argument*)args)->ThreadID;
+	int Rows = ((Argument*)args)->Rows;
+	int Cols = ((Argument*)args)->Cols;
+	DataType* MatrixA_ = ((Argument*)args)->MatA_;
+	DataType* MatrixB_ = ((Argument*)args)->MatB_;
+	DataType* MatrixC_ = ((Argument*)args)->MatC_;
+
+	int iStart = ThreadID*Rows/GlobalThreads;
+	int iEnd = (ThreadID+1)*Rows/GlobalThreads;
+
+	cout << "This is thread no. " << ThreadID << endl;
+	cout << "Range = " << iStart << "-" << iEnd-1 << endl;
+
+	for (int i=iStart; i<iEnd; i++)
+	{
+		for (int j=0; j<Cols; j++)
+		{
+			for (int k=0; k<Cols; k++)
+				MatrixC(i,j) = MatrixC(i,j) + MatrixA(i,j) * MatrixB(i,j);
+		}
+	}
+#ifndef WIN32
+	return NULL;
+#endif
+}
