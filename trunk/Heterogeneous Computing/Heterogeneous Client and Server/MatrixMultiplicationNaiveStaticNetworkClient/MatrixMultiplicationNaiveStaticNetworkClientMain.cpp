4b9216748b24ce52b3808911e2bf950ea413285e
infinity.ethereal <infinity.ethereal@ef33575d-811c-aa3e-9c56-0e3b2be09934>
2013-09-24 01:07:44 +0000

    [Heterogeneous Computing] Heterogeneous Client and Server: - Moved MultiThreaded TCP Server into Heterogeneous Client and Server folder and added client as well.

---
+++ trunk/Heterogeneous Computing/Heterogeneous Client and Server/MatrixMultiplicationNaiveStaticNetworkClient/MatrixMultiplicationNaiveStaticNetworkClientMain.cpp
@@ -0,0 +1,347 @@
+#if defined __linux__ || defined __CYGWIN__
+#include <pthread.h>
+#define TRET_TYPE void*
+#include <sys/time.h>
+#else
+#include <process.h>
+#define TRET_TYPE void
+#endif
+
+#include <Client.h>
+#include <MatrixMultiplicationNaiveGPU.hpp>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+const unsigned int Rows = 4U;
+const unsigned int Cols = 4U;
+const unsigned int Iterations = 10U;
+
+TRET_TYPE DeviceMultiplicationThread (void *);
+#if defined __linux__ || defined __CYGWIN__
+pthread_mutex_t PlatformMutexLock = PTHREAD_MUTEX_INITIALIZER;
+#else
+HANDLE PlatformMutexLock = CreateMutex (NULL, FALSE, NULL);
+#endif
+
+class DeviceArgument
+{
+	public:
+	unsigned int Rows;
+	unsigned int Cols;
+	PRECISION* MatA_;
+	PRECISION* MatB_;
+	PRECISION* MatC_;
+	unsigned int ThreadStart;
+	unsigned int ThreadEnd;
+	unsigned int Platform;
+	unsigned int Emulation;
+	unsigned int DeviceID;
+	unsigned int Iterations;
+	char DeviceType[4];
+};
+
+class SimulationParameters
+{
+	public:
+	unsigned int Rows;
+	unsigned int Cols;
+	unsigned int ThreadStart;
+	unsigned int ThreadEnd;
+	unsigned int Platform;
+	unsigned int Emulation;
+	unsigned int DeviceID;
+	unsigned int Iterations;
+	char DeviceType[4];
+};
+
+void InputRandom(PRECISION* Matrix_, unsigned int, unsigned int);
+void Display(PRECISION* Matrix_, unsigned int, unsigned int);
+void Initialise(PRECISION* Matrix_, unsigned int, unsigned int);
+void Multiply(PRECISION*, PRECISION*, PRECISION*, unsigned int, unsigned int);
+void Compare(PRECISION*, PRECISION*, unsigned int, unsigned int, unsigned int, unsigned int, PRECISION Multiplier);
+
+int main(int argc, char * argv[])
+{
+	// Standard error checking. Must provide server name/IP and port to connect.
+	if ( argc < 4 )
+	{
+		cout << "ERROR000: Incorrect input arguments." << endl;
+		cout << "Usage: './Client [server name or IP] [server port] [client port]'" << endl;
+		cout << "OR" << endl;
+		cout << "if using Makefile: 'make runC ARG=\"[server name or IP] [server port] [client port]\"'" << endl;
+		exit (-1);
+	}
+
+	int ClientPort = atoi(argv[3]);
+
+	// Create Client object.
+	CClient ClientObj (TCPSOCKET, ClientPort);			// Can create an object without arguments. Defaults are TCPSOCKET and DEFAULTPORT (6001).
+
+	// Create Client Socket.
+	ClientObj.CreateSocket (TCPSOCKET);
+	ClientObj.SetSocketOptions ();
+
+	// Initialize and bind Client address.
+	ClientObj.InitializeAddress (ClientPort);	// Without any argument default port will be chosen.
+	ClientObj.Bind ();
+
+	// Connect to Server. Server name/IP and port are provided as arguments.
+	ClientObj.Connect (argv[1], atoi (argv[2]));
+
+	double Computation = 1.f/2.4f;
+	const char ID[] = "C2D E8400 @3.00 GHz";
+
+	// Send and receive.
+	ClientObj.Send ((void *)&Computation, sizeof(double));
+
+	ClientObj.Receive();
+	ClientObj.CheckServerACK();
+
+	ClientObj.Send ((void *)ID, sizeof(ID));
+	ClientObj.CheckServerACK();
+
+	while (true)
+	{
+		ClientObj.Receive();
+		// Decode instruction.
+		if (!strncmp(ClientObj.GetBuffer(), "X00S", 4U))
+		{
+			SimulationParameters Sim;
+			ClientObj.Send((void *)&"ACK", 3U);
+			ClientObj.Receive((void*)&Sim, sizeof(Sim));
+			ClientObj.Send((void *)&"ACK", 3U);
+
+			srand(0);
+			PRECISION* MatrixA_ = new PRECISION[Sim.Rows*Sim.Cols];
+			PRECISION* MatrixB_ = new PRECISION[Sim.Rows*Sim.Cols];
+			PRECISION* MatrixC_ = new PRECISION[Sim.Rows*Sim.Cols];
+			PRECISION* MatrixCStandard_ = new PRECISION[Sim.Rows*Sim.Cols];
+			InputRandom(MatrixB_, Sim.Rows, Sim.Cols);
+
+			// Matrix A transfer
+			ClientObj.Receive();
+			if (!strncmp(ClientObj.GetBuffer(), "X00A", 4U))
+			{
+				ClientObj.Send((void *)&"ACK", 3U);
+				ClientObj.ReceiveData((void*)MatrixA_);
+				/*
+				unsigned int DataSize;
+				ClientObj.Receive((void*)&DataSize, sizeof(DataSize));
+				ClientObj.Send((void *)&"ACK", 3U);
+
+				cout << "Data size: " << DataSize << endl;
+
+				// Transfer Loop.
+				unsigned int BytesReceived = 0;
+				while (BytesReceived != DataSize)
+					BytesReceived += (unsigned int)ClientObj.Receive((void*)((char*)MatrixA_+BytesReceived), DataSize-BytesReceived);
+
+				cout << "Matrix A received." << endl;
+				ClientObj.Send((void *)&"ACK", 3U);
+
+				//Display(MatrixA_, Sim.Rows, Sim.Cols);
+				*/
+				Compare(MatrixB_, MatrixA_, Sim.Rows, Sim.Cols, 0U, Sim.Rows, (PRECISION)1U);
+			}
+			else
+				break;
+		}
+
+	}
+
+	srand((unsigned int)time(NULL));
+	srand(0);
+	PRECISION* MatrixA_ = new PRECISION[Rows*Cols];
+	PRECISION* MatrixB_ = new PRECISION[Rows*Cols];
+	PRECISION* MatrixC_ = new PRECISION[Rows*Cols];
+	PRECISION* MatrixCStandard_ = new PRECISION[Rows*Cols];
+
+	InputRandom(MatrixA_, Rows, Cols);
+	InputRandom(MatrixB_, Rows, Cols);
+	Initialise(MatrixC_, Rows, Cols);
+	Initialise(MatrixCStandard_, Rows, Cols);
+
+	unsigned int NumberOfDevices = 2U;
+	#if defined __linux__ || defined __CYGWIN__
+	std::cout << "Running on linux..." << std::endl;
+	pthread_t* threads = new pthread_t[NumberOfDevices];
+	#else
+	std::cout << "Running on windows..." << std::endl;
+	HANDLE* th = new HANDLE[NumberOfDevices];
+	#endif
+
+	DeviceArgument* Device = new DeviceArgument[NumberOfDevices];
+
+	// ========================= GPU ======================
+	Device[0].Rows = Rows;
+	Device[0].Cols = Cols;
+	Device[0].MatA_ = MatrixA_;
+	Device[0].MatB_ = MatrixB_;
+	Device[0].MatC_ = MatrixC_;
+	Device[0].ThreadStart = 48U;
+	Device[0].ThreadEnd = 1024U;
+	Device[0].Platform = 2U;
+	Device[0].Emulation = 2U;
+	Device[0].DeviceID = 0U;
+	Device[0].Iterations = Iterations;
+	strcpy(Device[0].DeviceType, "GPU");
+	// ========================= CPU ======================
+	Device[1].Rows = Rows;
+	Device[1].Cols = Cols;
+	Device[1].MatA_ = MatrixA_;
+	Device[1].MatB_ = MatrixB_;
+	Device[1].MatC_ = MatrixC_;
+	Device[1].ThreadStart = 0U;
+	Device[1].ThreadEnd = 48U;
+	Device[1].Platform = 1U;
+	Device[1].Emulation = 1U;
+	Device[1].DeviceID = 1U;
+	Device[1].Iterations = Iterations;
+	strcpy(Device[1].DeviceType, "CPU");
+
+	__int64 tStart, tEnd;
+	cout << "Dispatching workload to devices..." << endl;
+	tStart = GetTimeus64();
+	for (unsigned int i=0; i<NumberOfDevices; i++)
+	{
+		#if defined __linux__ || defined __CYGWIN__
+		pthread_create (&threads[i], NULL, DeviceMultiplicationThread, (void*)&Device[i]);
+		#else
+		th[i] = (HANDLE)_beginthread (DeviceMultiplicationThread, 0, (void*)&Device[i]);
+		#endif
+	}
+	for (unsigned int i=0; i<NumberOfDevices; i++)
+	{
+		#if defined __linux__ || defined __CYGWIN__
+		pthread_join (threads[i], NULL);
+		#else
+		WaitForSingleObject(th[i], INFINITE);
+		#endif
+	}
+	tEnd = GetTimeus64();
+	cout << "Time taken by all devices = " << ((double)(tEnd-tStart))/(1000000.) << " seconds." << endl;
+
+	cout << "Performing standard multiplication..." << endl;
+	Multiply(MatrixA_, MatrixB_, MatrixCStandard_, Rows, Cols);
+	cout << "Comparing results..." << endl;
+	Compare(MatrixCStandard_, MatrixC_, Rows, Cols, 0U, 1024U, (PRECISION)Iterations);
+
+	//Display(MatrixC_, Rows, Cols);
+	//Display(MatrixCStandard_, Rows, Cols);
+
+	delete[] MatrixA_;
+	delete[] MatrixB_;
+	delete[] MatrixC_;
+	delete[] MatrixCStandard_;
+
+	// Thread cleanup.
+	#if defined __linux__ || defined __CYGWIN__
+	delete[] threads;
+	#else
+	delete[] th;
+	#endif
+
+	ClientObj.CloseClientSocket ();
+
+	return 0;
+}
+
+#define Matrix(i,j) Matrix_[Cols*(i)+(j)]
+#define MatrixA(i,j) MatrixA_[Cols*(i)+(j)]
+#define MatrixB(i,j) MatrixB_[Cols*(i)+(j)]
+#define MatrixC(i,j) MatrixC_[Cols*(i)+(j)]
+
+void InputRandom(PRECISION* Matrix_, unsigned int Rows, unsigned int Cols)
+{
+	for (unsigned int i=0; i<Rows; i++)
+	{
+		for (unsigned int j=0; j<Cols; j++)
+			Matrix(i,j) = (PRECISION)rand()/rand();
+	}
+}
+
+void Display(PRECISION* Matrix_, unsigned int Rows, unsigned int Cols)
+{
+	for (unsigned int i=0; i<Rows; i++)
+	{
+		for (unsigned int j=0; j<Cols; j++)
+			cout << setprecision(3) << setw(5) << Matrix(i,j) << "\t";
+		cout << endl;
+	}
+}
+
+void Initialise(PRECISION* Matrix_, unsigned int Rows, unsigned int Cols)
+{
+	for (unsigned int i=0; i<Rows; i++)
+	{
+		for (unsigned int j=0; j<Cols; j++)
+			Matrix(i,j) = (PRECISION)0;
+	}
+}
+
+void Multiply(PRECISION* MatrixA_, PRECISION* MatrixB_, PRECISION* MatrixC_, unsigned int Rows, unsigned int Cols)
+{
+	for (unsigned int i=0; i<Rows; i++)
+	{
+		for (unsigned int j=0; j<Cols; j++)
+		{
+			for (unsigned int k=0; k<Cols; k++)
+				MatrixC(i,j) = MatrixC(i,j) + MatrixA(i,k) * MatrixB(k,j);
+		}
+	}
+}
+
+void Compare(PRECISION* MatrixA_, PRECISION* MatrixB_, unsigned int Rows, unsigned int Cols, unsigned int ThreadStart, unsigned int ThreadEnd, PRECISION Multiplier)
+{
+	for (unsigned int i=ThreadStart; i<ThreadEnd; i++)
+	{
+		for (unsigned int j=0; j<Cols; j++)
+		{
+			const PRECISION Difference = MatrixA(i,j) - MatrixB(i,j)/Multiplier;
+			if (Difference < (PRECISION)-0.05 || Difference > (PRECISION)0.05)
+			{
+				cout << "Results don't match!" << endl;
+				cout << "i = " << i << ", j = " << j << endl;
+				return;
+			}
+		}
+	}
+	cout << "Results verified!" << endl;
+}
+
+TRET_TYPE DeviceMultiplicationThread (void *DeviceArgs)
+{
+	cout << "Rows: " << ((DeviceArgument*)DeviceArgs)->Rows;
+	cout << "Cols: " << ((DeviceArgument*)DeviceArgs)->Cols;
+	cout << "Thread Start: " << ((DeviceArgument*)DeviceArgs)->ThreadStart;
+	cout << "Thread End: " << ((DeviceArgument*)DeviceArgs)->ThreadEnd;
+	cout << "Platform: " << ((DeviceArgument*)DeviceArgs)->Platform;
+	cout << "Emulation: " << ((DeviceArgument*)DeviceArgs)->Emulation;
+	cout << "Iterations: " << ((DeviceArgument*)DeviceArgs)->Iterations;
+
+	CMatrixMultiplicationNaiveGPU HeterogeneousSim(((DeviceArgument*)DeviceArgs)->Rows, ((DeviceArgument*)DeviceArgs)->Cols, ((DeviceArgument*)DeviceArgs)->MatA_, ((DeviceArgument*)DeviceArgs)->MatB_, ((DeviceArgument*)DeviceArgs)->MatC_, ((DeviceArgument*)DeviceArgs)->ThreadStart, ((DeviceArgument*)DeviceArgs)->ThreadEnd);
+	HeterogeneousSim.StartTimer();
+
+	#if defined __linux__ || defined __CYGWIN__
+	pthread_mutex_lock (&PlatformMutexLock);
+	#else
+	WaitForSingleObject (PlatformMutexLock, INFINITE);
+	#endif
+	// CL initialisation must be atomic.
+	HeterogeneousSim.InitialiseCL(((DeviceArgument*)DeviceArgs)->Platform, ((DeviceArgument*)DeviceArgs)->Emulation);
+	#if defined __linux__ || defined __CYGWIN__
+	pthread_mutex_unlock (&PlatformMutexLock);
+	#else
+	ReleaseMutex (PlatformMutexLock);
+	#endif
+
+	HeterogeneousSim.CompleteRunHeterogeneous(Iterations); // Complete heterogeneous run.
+	HeterogeneousSim.StopTimer();
+	cout << "Total time taken by " << ((DeviceArgument*)DeviceArgs)->DeviceType << " with device ID " << ((DeviceArgument*)DeviceArgs)->DeviceID << " = " << HeterogeneousSim.GetElapsedTime() << " seconds." << endl;
+
+#ifndef WIN32
+	return NULL;
+#endif
+}
---
+++ trunk/Heterogeneous Computing/Heterogeneous Client and Server/MultiThreaded TCP Server/Server/src/Server.cpp
@@ -0,0 +1,375 @@
+#include <Server.h>
+#include <cstring>
+#include <iostream>
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::fill;
+
+CServer::CServer (int pType, int pServerPort): Type (pType), ServerPort (pServerPort)
+{
+	#ifdef WIN32
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(1, 1), &wsaData) != 0)
+	{
+		cerr << "WSAStartup failed." << endl;
+		exit(1);
+	}
+	#endif
+	cout << "Server object created with type " << (Type == TCPSOCKET ? "TCP" : "UDP") << " and port " << ServerPort << endl;
+	for (int i=0; i<MAXCLIENTS; i++)
+		ClientSocketFD[i] = -1;
+	Connections = 0;
+}
+
+int CServer::CreateSocket (int pType)			// 0 = TCP, 1 = UDP; default is to create TCP socket.
+{
+	int errorcheck;
+	if (pType == TCPSOCKET)
+	{
+		Type = TCPSOCKET;
+		errorcheck = ServerSocketFD = (int)socket (AF_INET, SOCK_STREAM, 0);
+	}
+	else
+	{
+		Type = UDPSOCKET;
+		errorcheck = ServerSocketFD = (int)socket (AF_INET, SOCK_DGRAM, 0);
+	}
+	if (errorcheck == -1)
+	{
+		cerr << "ERROR001: Creating socket of type " << pType << endl;
+	}
+	return errorcheck;
+}
+
+int CServer::SetSocketOptions ()					// Set socket options to reuse address.
+{
+	int Yes = 1;
+	int errorcheck;
+	errorcheck = setsockopt (ServerSocketFD, SOL_SOCKET, SO_REUSEADDR, (char *)&Yes, sizeof (int));
+	if (errorcheck == -1)
+	{
+		cerr << "ERROR002: Setting socket options. " << endl;
+	}
+	return errorcheck;
+}
+
+int CServer::InitializeAddress (int pPort)	// Default Server port is 5000.
+{
+	ServerPort = pPort;
+	// Server address initialization for binding.
+	ServerAddress.sin_family = AF_INET;				// Socekt family.
+	ServerAddress.sin_addr.s_addr = INADDR_ANY;		// Setting server IP. INADDR_ANY blank IP.
+	ServerAddress.sin_port = htons (ServerPort);	// Setting server port.
+	fill ((char*)&(ServerAddress.sin_zero), (char*)&(ServerAddress.sin_zero)+8, '\0');
+	return 0;
+}
+
+int CServer::Bind ()								// Bind Server socket with address.
+{
+	int errorcheck;
+	errorcheck = bind (ServerSocketFD, (sockaddr *)&ServerAddress, sizeof (ServerAddress));
+	if (errorcheck == -1)
+	{
+		cerr << "ERROR003: Binding." << endl;
+	}
+	return errorcheck;
+}
+
+int CServer::Listen ()								// Listen for incoming connections; for TCP Server.
+{
+	int errorcheck;
+	errorcheck = listen (ServerSocketFD, 0);
+	if (errorcheck == -1)
+	{
+		cerr << "ERROR004: Listening." << endl;
+	}
+	return errorcheck;
+}
+
+// Returns index of connected client.
+int CServer::Accept ()								// Accept incoming connections.
+{
+	socklen_t ClientAddressLength = sizeof (sockaddr_in);
+	int FreeClientSocket = -1;
+	if (Connections < MAXCLIENTS)
+	{
+		for (int i=0; i<MAXCLIENTS; i++)
+		{
+			if (ClientSocketFD[i] == -1)
+			{
+				FreeClientSocket = i;
+				break;
+			}
+		}
+		int errorcheck = ClientSocketFD[FreeClientSocket] = (int)accept (ServerSocketFD, (sockaddr *)&ClientAddress[FreeClientSocket], &ClientAddressLength);
+		if (errorcheck == -1)
+		{
+			cerr << "ERROR005: Accepting." << endl;
+			return errorcheck;
+		}
+		cout << "*** Server got connection from " << inet_ntoa (ClientAddress[FreeClientSocket].sin_addr) << " on socket '" << ClientSocketFD[FreeClientSocket] << "' ***" << endl;
+	}
+	return FreeClientSocket;
+}
+
+int CServer::Receive (int ClientIndex)
+{
+	if (ClientIndex < 0 || ClientIndex >= MAXCLIENTS)
+	{
+		cerr << "ERROR06A Client index out of bound." << endl;
+		return -1;
+	}
+	if (ClientSocketFD[ClientIndex] == -1)
+	{
+		cerr << "ERROR06B Invalid client socket file descriptor number." << endl;
+		return -1;
+	}
+
+	errorcheck[ClientIndex] = NumOfBytesReceived[ClientIndex] = recv (ClientSocketFD[ClientIndex], Buffer[ClientIndex], MAXBUFFERSIZE-1, 0);
+	if (errorcheck[ClientIndex] == -1)
+	{
+		cerr << "ERROR006 Receiving" << endl;
+		return errorcheck[ClientIndex];
+	}
+	Buffer[ClientIndex][NumOfBytesReceived[ClientIndex]] = '\0';
+	return errorcheck[ClientIndex];
+}
+
+int CServer::Receive (void* DataBuffer, unsigned DataSize, int ClientIndex)
+{
+	int errorcheck = NumOfBytesReceived[ClientIndex] = recv (ClientSocketFD[ClientIndex], (char*)DataBuffer, DataSize, 0);
+	if (errorcheck == -1)
+	{
+		cerr << "ERROR005 Receiveing" << endl;
+	}
+	return errorcheck;
+}
+
+int CServer::Send (void *Data, unsigned int DataSize, int ClientIndex)
+{
+	if (ClientIndex < 0 || ClientIndex >= MAXCLIENTS)
+	{
+		cerr << "ERROR07A Client index out of bound." << endl;
+		return -1;
+	}
+	if (ClientSocketFD[ClientIndex] == -1)
+	{
+		cerr << "ERROR07B Invalid client socket file descriptor number." << endl;
+		return -1;
+	}
+
+	errorcheck[ClientIndex] = NumOfBytesSent[ClientIndex] = send (ClientSocketFD[ClientIndex], (char *)Data, DataSize, 0);
+	if (errorcheck[ClientIndex] == -1)
+	{
+		cerr << "ERROR007: Server Sending. " << endl;
+		return errorcheck[ClientIndex];
+	}
+	return errorcheck[ClientIndex];
+}
+
+int CServer::SendData(void* Data, unsigned int DataSize, int ClientIndex)
+{
+	Send((void*)&DataSize, sizeof(DataSize), ClientIndex);
+	Receive(ClientIndex);
+
+	cout << "Data size: " << DataSize << endl;
+
+	unsigned int BytesSent = 0;
+	while (BytesSent != DataSize)
+		BytesSent += (unsigned int)Send((void*)((char*)Data+BytesSent), DataSize-BytesSent, ClientIndex);
+
+	cout << "Data Sent." << endl;
+	Receive(ClientIndex);
+	if (!strncmp("ACK", GetBuffer(ClientIndex), 3U))
+		cout << "ACK received." << endl;
+
+	return BytesSent;
+}
+
+int CServer::ReceiveData(void* Data, int ClientIndex)
+{
+	unsigned int DataSize;
+	Receive((void*)&DataSize, sizeof(DataSize), ClientIndex);
+	Send((void *)&"ACK", 3U, ClientIndex);
+
+	cout << "Data size: " << DataSize << endl;
+
+	// Transfer Loop.
+	unsigned int BytesReceived = 0;
+	while (BytesReceived != DataSize)
+		BytesReceived += (unsigned int)Receive((void*)((char*)Data+BytesReceived), DataSize-BytesReceived, ClientIndex);
+
+	cout << "Matrix A received." << endl;
+	Send((void *)&"ACK", 3U, ClientIndex);
+
+	return BytesReceived;
+}
+
+// UDP, sendto (data, datasize, IP/name, port);
+int CServer::SendTo (void *Data, unsigned int DataSize)
+{
+	errorcheck[0] = NumOfBytesSent[0] = sendto (ServerSocketFD, (char *)Data, DataSize, 0, (sockaddr *)&TheirAddress, sizeof (TheirAddress));
+	if (errorcheck[0] == -1)
+	{
+		cerr << "ERROR008: Server Sending to. " << endl;
+	}
+	return errorcheck[0];
+}
+int CServer::SendTo (void *Data, unsigned int DataSize, char * pTheirIP, int pTheirPort)
+{
+	struct hostent* TheirIP;		// Server name/IP.
+	if ((TheirIP = gethostbyname (pTheirIP)) == NULL)
+	{
+		cerr << "ERROR009: Getting Their name/IP" << endl;
+		return -1;
+	}
+	// Initializing Their address to send to.
+	TheirAddress.sin_family = AF_INET;							// Socket family.
+	TheirAddress.sin_addr = *((in_addr *)(*TheirIP).h_addr);		// Their name/IP.
+	TheirAddress.sin_port = htons (pTheirPort);			// Their port provided as argument.
+	fill ((char*)&(TheirAddress.sin_zero), (char*)&(TheirAddress.sin_zero)+8, '\0');
+
+	errorcheck[0] = NumOfBytesSent[0] = sendto (ServerSocketFD, (char *)Data, DataSize, 0, (sockaddr *)&TheirAddress, sizeof (TheirAddress));
+	if (errorcheck[0] == -1)
+	{
+		cerr << "ERROR010: Server Sending to. " << endl;
+	}
+	return errorcheck[0];
+}
+// recvfrom ();
+int CServer::RecvFrom ()
+{
+	socklen_t TheirAddressSize = sizeof (TheirAddress);
+	int errorcheck = NumOfBytesReceived[0] = recvfrom (ServerSocketFD, Buffer[0], MAXBUFFERSIZE-1, 0, (sockaddr *)&TheirAddress, &TheirAddressSize);
+	if (errorcheck == -1)
+	{
+		cerr << "ERROR011 Receiveing" << endl;
+		return errorcheck;
+	}
+	Buffer[0][NumOfBytesReceived[0]] = '\0';
+	cout << "Server got packet from " << inet_ntoa (TheirAddress.sin_addr) << " on socket " << ServerSocketFD << endl;
+	return errorcheck;
+}
+
+int CServer::CloseServerSocket ()
+{
+	int errorcheck = close (ServerSocketFD);
+	return errorcheck;
+}
+
+int CServer::CloseClientSocket (int ClientIndex)
+{
+	if (ClientIndex < 0 || ClientIndex >= MAXCLIENTS)
+	{
+		cerr << "ERROR06A Client index out of bound." << endl;
+		return -1;
+	}
+	if (ClientSocketFD[ClientIndex] == -1)
+	{
+		cerr << "ERROR06B Invalid client socket file descriptor number." << endl;
+		return -1;
+	}
+
+	errorcheck[ClientIndex] = close (ClientSocketFD[ClientIndex]);
+	ClientSocketFD[ClientIndex] = -1;
+
+	return errorcheck[ClientIndex];
+}
+
+int CServer::CloseAllClientSockets()
+{
+	for (int i=0; i<MAXCLIENTS; i++)
+	{
+		if (ClientSocketFD[i] != -1)
+		{
+			errorcheck[i] = close (ClientSocketFD[i]);
+			ClientSocketFD[i] = -1;
+		}
+	}
+	return 0;
+}
+
+// Additional Functions.
+int CServer::DisplayServerInfo ()
+{
+	cout << "Server Address: " << inet_ntoa (ServerAddress.sin_addr) << endl;
+	cout << "Server Port   : " << ntohs (ServerAddress.sin_port) << endl;
+	cout << "Server Socket : " << ServerSocketFD << endl;
+	return 0;
+}
+
+int CServer::DisplayClientInfo (int ClientIndex)
+{
+	if (ClientIndex < 0 || ClientIndex >= MAXCLIENTS)
+	{
+		cerr << "ERROR06A Client index out of bound." << endl;
+		return -1;
+	}
+	if (ClientSocketFD[ClientIndex] == -1)
+	{
+		cerr << "ERROR06B Invalid client socket file descriptor number." << endl;
+		return -1;
+	}
+
+	cout << "Client Address: " << inet_ntoa (ClientAddress[ClientIndex].sin_addr) << endl;
+	cout << "Client Port   : " << ntohs (ClientAddress[ClientIndex].sin_port) << endl;
+	cout << "Client Socket : " << ClientSocketFD[ClientIndex] << endl;
+	cout << "Client ID     : " << ClientsInfo[ClientIndex].ID << endl;
+	cout << "Computation   : " << ClientsInfo[ClientIndex].ComputationPower << endl;
+	return 0;
+}
+
+int CServer::DisplayAllClientsInfo ()
+{
+	for (int i=0; i<MAXCLIENTS; i++)
+	{
+		if (ClientSocketFD[i] != -1)
+		{
+			cout << "Client index  : " << endl;
+			cout << "Client Address: " << inet_ntoa (ClientAddress[i].sin_addr) << endl;
+			cout << "Client Port   : " << ntohs (ClientAddress[i].sin_port) << endl;
+			cout << "Client Socket : " << ClientSocketFD[i] << endl;
+			cout << "Client ID     : " << ClientsInfo[i].ID << endl;
+			cout << "Computation   : " << ClientsInfo[i].ComputationPower << endl;
+		}
+	}
+	return 0;
+}
+int CServer::DisplayTheirInfo ()
+{
+	cout << "Their Address: " << inet_ntoa (TheirAddress.sin_addr) << endl;
+	cout << "Their Port   : " << ntohs (TheirAddress.sin_port) << endl;
+	return 0;
+}
+
+int CServer::AcceptClient()
+{
+	int ClientIndex = Accept ();
+	Receive(ClientIndex); // Receive computation power of client.
+	double* Computation;
+	Computation = (double*)GetBuffer(ClientIndex);
+	ClientsInfo[ClientIndex].ComputationPower = *Computation;
+	Send((void *)&"ACK", 3U, ClientIndex);
+	Receive(ClientIndex); // Receive client ID.
+	Send((void *)&"ACK", 3U, ClientIndex);
+	strncpy(ClientsInfo[ClientIndex].ID, GetBuffer(ClientIndex), NumOfBytesReceived[ClientIndex]);
+	DisplayClientInfo (ClientIndex);
+
+	return ClientIndex;
+}
+
+int CServer::GetNumOfBytesSent (int ClientIndex)
+{
+	return NumOfBytesSent[ClientIndex];
+}
+
+int CServer::GetNumOfBytesReceived (int ClientIndex)
+{
+	return NumOfBytesReceived[ClientIndex];
+}
+
+char* CServer::GetBuffer (int ClientIndex)
+{
+	return Buffer[ClientIndex];
+}
