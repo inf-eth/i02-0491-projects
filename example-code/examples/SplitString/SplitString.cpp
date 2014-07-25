b6a5237b01b7696b1b5075851a8f59631b798284
infinity.ethereal <infinity.ethereal@ef33575d-811c-aa3e-9c56-0e3b2be09934>
2014-07-25 10:44:02 +0000

    [example-code] - Standardised output directory as $(Configuration)\$(Platform)\ for all windows projects to be consistent with linux. - Added a new project to tokenise/split a string written exclusively in C++.

---
+++ example-code/examples/SplitString/SplitString.cpp
@@ -0,0 +1,47 @@
+// Reference: Taken from http://stackoverflow.com/questions/236129/how-to-split-a-string-in-c?rq=1
+// Example Usage:
+// vector<string> v = split<string>("Hello, there; World", ";,");
+// vector<wstring> v = split<wstring>(L"Hello, there; World", L";,");
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+template<typename T> vector<T> split(const T & str, const T & delimiters)
+{
+	vector<T> v;
+	T::size_type start = 0;
+	auto pos = str.find_first_of(delimiters, start);
+	while(pos != T::npos)
+	{
+		if(pos != start) // ignore empty tokens
+			v.emplace_back(str.substr(start, pos - start));
+		start = pos + 1;
+		pos = str.find_first_of(delimiters, start);
+	}
+	if(start < str.length()) // ignore trailing delimiter
+		v.emplace_back(str.substr(start, str.length() - start)); // add what's left of the string
+	return v;
+}
+
+int main()
+{
+	string s;
+	string delim;
+
+	cout << "Enter a string to split: ";
+	getline(cin, s);
+
+	cout << "Enter delimiters: ";
+	getline(cin, delim);
+
+	vector<string> v = split<string>(s, delim);
+
+	cout << "Tokens are: ";
+	for (unsigned int i=0; i<v.size(); i++)
+		cout << v[i] << endl;
+	cout << endl;
+
+	return 0;
+}
