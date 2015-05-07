704786dbfa2a85912e01143c380fcdd96343623b
infinity.ethereal <infinity.ethereal@ef33575d-811c-aa3e-9c56-0e3b2be09934>
2015-05-07 06:24:11 +0000

    Network Programming: - Updated TCP Example code.

---
+++ A Guide to Network Programming Using C++/TCP Example/Server/src/ServerMain.cpp
@@ -0,0 +1,62 @@
+#include <Server.h>
+#include <cstring>
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	// Create Server object.
+	Server ServerObj;
+
+	// Create Server socket and set socket options.
+	ServerObj.CreateSocket(TCPSOCKET);
+	ServerObj.SetSocketOptions();
+
+	// Initialise Server address struct and bind it with Server's socket.
+	ServerObj.InitialiseAddress(6000);
+	ServerObj.Bind();
+
+	// Listen for incoming connections.
+	ServerObj.Listen();
+	ServerObj.DisplayServerInfo();
+
+	// Accept any incoming connections.
+	ServerObj.Accept();
+	ServerObj.DisplayClientInfo();
+
+	// Send and receive message.
+	ServerObj.Receive();
+	cout << "No. of bytes received: " << ServerObj.GetNumOfBytesReceived() << endl;
+	cout << "Client says: " << ServerObj.GetBuffer() << endl;
+
+	char message[] = "Hello from server.";
+	ServerObj.Send((void*)message, (unsigned int)strlen(message));
+
+	// Send a number.
+	int x = 25;
+	ServerObj.Send((void*)&x, sizeof(x));
+
+	// Send an array.
+	int Data[5] = {12, 33, -1, 4, 21};
+	ServerObj.Send((void*)&Data, sizeof(Data));
+
+	// Send a structure.
+	class Complex
+	{
+	public:
+		float real;
+		float img;
+	};
+	Complex C;
+	C.real = 3.1f;
+	C.img = -1.4f;
+	ServerObj.Send((void*)&C, sizeof(C));
+
+	// Close sockets.
+	ServerObj.CloseClientSocket();
+	ServerObj.CloseServerSocket();
+
+	return 0;
+}
